e353d38c854892841f880465cfb9fdc33b1fb935
maryo1234 <olearym@landcareresearch.co.nz>
2026-03-30 16:23:56 +1300

    MWDS-279 use enum instead of dict object

--- ckanext/datasetapproval/enums.py
+++ ckanext/datasetapproval/enums.py
@@ -1,25 +1,25 @@
-VOCABS = {
-    "rejection_reason": {
-        "data_quality": "Data quality issues",
-        "documentation": "Incomplete or inadequate documentation",
-        "legal": "Legal / contractual restrictions",
-        "cultural": "Cultural / ethical concerns",
-        "privacy": "Privacy / sensitivity issues",
-        "ip": "IP / commercial risk",
-        "sop": "Non-compliance with SOP",
-        "other": "Other",
-    },
-    "resubmission": {
-        "yes": "Yes",
-        "no": "No",
-    },
-    "compliance_status": {
-        "non_compliant": "Non-compliant",
-        "compliant": "Compliant",
-        "partial": "Partially compliant",
-    },
-    "approval_outcome": {
-        "approved": "Approved",
-        "conditional": "Conditional Approval"
-    }
-}
+from enum import Enum
+
+class RejectionReason(str, Enum):
+    data_quality = "Data quality issues"
+    documentation = "Incomplete or inadequate documentation"
+    legal = "Legal / contractual restrictions"
+    cultural = "Cultural / ethical concerns"
+    privacy = "Privacy / sensitivity issues"
+    ip = "IP / commercial risk"
+    sop = "Non-compliance with SOP"
+    other = "Other"
+
+class ApprovalOutcome(str, Enum):
+    approved = "Approved"
+    conditional = "Approved with conditions"
+
+class ComplianceStatus(str, Enum):
+    non_compliant = "Non-compliant"
+    compliant = "Compliant"
+    partial = "Partially compliant"
+
+class VOCAB_ENUMS:
+    rejection_reason = RejectionReason
+    approval_outcome = ApprovalOutcome
+    compliance_status = ComplianceStatus
